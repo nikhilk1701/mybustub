a803b1e7a4f17451311e73a98896b46cdc458a0e
Nikhil <nikhil@nikhils-air.mynetworksettings.com>
2023-12-24 01:14:54 -0500

    Primer/sql_string_functions: implemented upper() and lower() functions

--- src/include/execution/expressions/string_expression.h
+++ src/include/execution/expressions/string_expression.h
@@ -46,7 +46,13 @@ class StringExpression : public AbstractExpression {
 
   auto Compute(const std::string &val) const -> std::string {
     // TODO(student): implement upper / lower.
-    return {};
+    std::string result = val;
+    if (expr_type_ == StringExpressionType::Lower) {
+      std::transform(val.begin(), val.end(), result.begin(), [](const char c) {return std::tolower(c);});
+    } else if (expr_type_ == StringExpressionType::Upper) {
+      std::transform(val.begin(), val.end(), result.begin(), [](const char c) {return std::toupper(c);});
+    }
+    return result;
   }
 
   auto Evaluate(const Tuple *tuple, const Schema &schema) const -> Value override {
--- src/planner/plan_func_call.cpp
+++ src/planner/plan_func_call.cpp
@@ -39,6 +39,16 @@ auto Planner::GetFuncCallFromFactory(const std::string &func_name, std::vector<A
   // 1. check if the parsed function name is "lower" or "upper".
   // 2. verify the number of args (should be 1), refer to the test cases for when you should throw an `Exception`.
   // 3. return a `StringExpression` std::shared_ptr.
+
+  if (func_name == "lower" || func_name == "upper") {
+    if (args.size() == 1) {
+      StringExpressionType expr_type = (func_name == "lower")? StringExpressionType::Lower : StringExpressionType::Upper;
+      std::cout << args[0]->ToString() << std::endl;
+      return std::make_shared<StringExpression>(args[0], expr_type);
+    }
+    throw Exception(fmt::format("expected 1 argument, recieved {}", args.size()));
+  }
+
   throw Exception(fmt::format("func call {} not supported in planner yet", func_name));
 }
 
