84d761f91971c7f678314df64cf0493bf4263a08
Nikhil <nikhil@nikhils-air.mynetworksettings.com>
2024-01-30 18:27:38 -0500

    P3: Implement sequence scan executor

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,13 +11,58 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include <memory>
+#include "buffer/buffer_pool_manager.h"
+#include "catalog/catalog.h"
+#include "catalog/schema.h"
+#include "concurrency/transaction.h"
+#include "recovery/log_manager.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) { 
+    plan_ = plan;
+    table_itr_ =  nullptr;
+}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+    table_oid_t table_oid = plan_->GetTableOid();
+    Catalog* catalog = exec_ctx_->GetCatalog();
+    TableInfo* table_info = catalog->GetTable(table_oid);
+    TableHeap* table_heap = table_info->table_.get();
+    TableIterator table_itr = table_heap->MakeIterator();
+    table_itr_ = &table_itr;
+    // get table metadata?
+    // initialize objects to read from table
+    // initialize counter 
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    // throw NotImplementedException("SeqScanExecutor is not implemented"); 
+}
+
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {  
+
+    // currently working
+    if (table_itr_->IsEnd()) {
+        return false;
+    }
+
+    TupleMeta tuple_mtd = table_itr_->GetTuple().first;
+    if (tuple_mtd.is_deleted_) {
+        return false;
+    }
+    *tuple = table_itr_->GetTuple().second;
+    *rid = table_itr_->GetRID();
+
+    Schema schema = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid())->schema_;
+
+    if (plan_->filter_predicate_->Evaluate(tuple, schema).IsNull()) {
+        return false;
+    }
+
+    table_itr_++;
+    return true;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/seq_scan_executor.h
+++ src/include/execution/executors/seq_scan_executor.h
@@ -12,11 +12,15 @@
 
 #pragma once
 
+#include <memory>
 #include <vector>
 
+#include "catalog/schema.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/seq_scan_plan.h"
+#include "storage/table/table_heap.h"
+#include "storage/table/table_iterator.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
@@ -50,5 +54,6 @@ class SeqScanExecutor : public AbstractExecutor {
  private:
   /** The sequential scan plan node to be executed */
   const SeqScanPlanNode *plan_;
+  TableIterator* table_itr_;
 };
 }  // namespace bustub
